caafeaf2f7413349638099f033a57f0f723e2c7e
matheus-costa <matheuscostafinsceca@gmail.com>
2025-01-26 23:01:20 -0300

    feat: definindo a classe responsável pelas funções de gerenciamento do banco de dados e do que será mostrado ao usuário

---
+++ projetos/app/Http/Controllers/ProjetosController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ModelProjeto;
+use App\Http\Requests\ProjetosRequest;
+
+
+class ProjetosController extends Controller
+{
+
+    private $projeto;
+
+    public function __construct(){
+       $this->projeto = new ModelProjeto();
+       
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        #dd($this->projeto->all());
+        $projeto = $this->projeto->all()->sortBy(callback:'nome');
+        return view('index', compact('projeto'));  
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $projeto=$this->projeto->all();
+        return view('create',compact('projeto'));  
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $cad=$this->projeto->create([
+            'nome'=>$request->nome,
+            'descricao'=>$request->descricao,
+            'data'=>$request->data,
+            'status'=>$request->status
+         ]);
+         if($cad){
+             return redirect('projetos');
+         }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+       
+        $projeto = $this->projeto->find($id);
+        return view('show',compact('projeto'));  
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
