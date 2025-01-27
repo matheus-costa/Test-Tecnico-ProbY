315f8c5a88b56cb6fd1d30405a5fb3d8d1dbfdde
matheus-costa <matheuscostafinsceca@gmail.com>
2025-01-26 22:56:47 -0300

    feat: definindo as rotas

--- projetos/routes/web.php
+++ projetos/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/projetos', [ProjetosController::class, 'index']);
+Route::get('/projetos/{id}', [ProjetosController::class, 'show'])->name('projetos.show');
+Route::get('/projetos/create', [ProjetosController::class, 'create'])->name('projetos.create');;
+Route::get('/projetos/{id}/edit', [ProjetosController::class, 'edit'])->name('projetos.edit');
+Route::delete('/projetos/{id}', [ProjetosController::class, 'destroy'])->name('projetos.destroy');
+
+
